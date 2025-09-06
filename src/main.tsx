fed13fa92ea4042882fbfb4293453a178dabf0b2
Henrique Melo <hmelo2509@gmail.com>
2025-09-06 21:55:51 +0000

    Add @vercel/analytics package and integrate analytics component into the main application render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Analytics />
+        <App />
+    </React.StrictMode>
+);
